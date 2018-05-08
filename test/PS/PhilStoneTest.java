0f85e12f0931189e7764e23334b7045ff7ac199e
Renzo Degiovanni <rdegiovanni@dc.exa.unrc.edu.ar>
2018-05-08 10:29:08 -0300

    minot change in the junit test

--- test/PS/PhilStoneTest.java
+++ test/PS/PhilStoneTest.java
@@ -12,7 +12,28 @@ public class PhilStoneTest {
 		String [] args = {"-scope=6", "-pdf", filename};
 		PhilStone.main(args);
 	}
+	
+	@Test
+	public void testBackery() {
+		String filename = "src/tests/bakery/bakery.spec";
+		String [] args = {"-scope=6", "-pdf", filename};
+		PhilStone.main(args);
+	}
+	
+	@Test
+	public void testPeterson() {
+		String filename = "src/tests/peterson/peterson.spec";
+		String [] args = {"-scope=6", "-pdf", filename};
+		PhilStone.main(args);
+	}
 
+	
+	@Test
+	public void testReaderWriter() {
+		String filename = "src/tests/readerwriter/reader2writer3.spec";
+		String [] args = {"-scope=14", "-pdf", filename};
+		PhilStone.main(args);
+	}
 	@Test
 	public void testPhilLiveness() {
 		String filename = "src/tests/phils/phil-liveness.spec";
